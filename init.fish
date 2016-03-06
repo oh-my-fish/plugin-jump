28819e6da87f146c63b79491e52d575322fe49f3
Justin Hileman <justin@justinhileman.info>
2016-03-06 13:06:27 -0800

    Initialize immediately, rather than waiting for an event.

--- init.fish
+++ init.fish
@@ -1,6 +1,4 @@
-function init -a path --on-event init_jump
-  if not set -q MARKPATH
-    set -gx MARKPATH $HOME/.marks
-    command mkdir -p $MARKPATH
-  end
+if not set -q MARKPATH
+  set -gx MARKPATH $HOME/.marks
+  command mkdir -p $MARKPATH
 end
