9b36be2a84392a5236361d4884e12ff39d1c4cd2
Ben Echols <lologarithm@gmail.com>
2019-01-23 11:28:41 -0700

    fix jump for new fish 3.0.0 virtual directory

--- functions/jump.fish
+++ functions/jump.fish
@@ -4,6 +4,7 @@ function jump
   else
     if test -d $MARKPATH/$argv[1] -a -L $MARKPATH/$argv[1]
       cd $MARKPATH/$argv[1]
+      cd (pwd -P)
     else
       echo "No such mark: $argv[1]"
     end
